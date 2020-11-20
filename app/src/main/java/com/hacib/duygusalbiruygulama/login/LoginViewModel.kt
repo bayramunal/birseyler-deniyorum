f593752d952f98b3e0bbab20728b30551b7ef3f2
Bayram ÜNAL <hacibayram.unal@gmail.com>
2020-11-20 23:35:57 +0300

    wrong formatted email and entering less than 6 characters of password check

--- app/src/main/java/com/hacib/duygusalbiruygulama/login/LoginViewModel.kt
+++ app/src/main/java/com/hacib/duygusalbiruygulama/login/LoginViewModel.kt
@@ -4,7 +4,6 @@ import android.view.View
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.google.firebase.auth.AuthResult
 import com.google.firebase.auth.FirebaseUser
 import com.hacib.duygusalbiruygulama.network.Constants
 import com.hacib.duygusalbiruygulama.network.NetworkListener
@@ -13,22 +12,37 @@ import com.hacib.duygusalbiruygulama.utils.Util.io
 class LoginViewModel(
     private val repository: LoginRepository
 ) : ViewModel() {
-    var userName : String ?= null
-    var password : String ?= null
+    var email: String? = null
+    var password: String? = null
     var user = MutableLiveData<FirebaseUser>()
-    var listener: NetworkListener ?= null
+    var listener: NetworkListener? = null
 
-    fun getUser() : LiveData<FirebaseUser> = user
+    fun getUser(): LiveData<FirebaseUser> = user
+
+    private fun checkEmailFormat(email: String): Boolean {
+        var atCondition = email.contains("@")
+        var domainCondition = false
+        if (atCondition) {
+            val substr = email.split("@")[1]
+            domainCondition = substr.contains(".")
+        }
+        return atCondition && domainCondition
+    }
+
+    private fun nullOrEmptyCheck(email: String, password: String) =
+        !email.isNullOrEmpty() && !password.isNullOrEmpty()
 
     fun loginBtnClick(view: View) {
-        println("username - password : $userName - $password")
+        println("username - password : $email - $password")
         listener?.onStart("login request ${Constants.NETWORK_START}")
 
-        var result : AuthResult ?= null
-
-        if (!userName.isNullOrEmpty() && !password.isNullOrEmpty()) {
+        if (
+            nullOrEmptyCheck(email!!, password!!)
+            && checkEmailFormat(email!!)
+            && (password!!.length >= 6)
+        ) {
             io {
-                result = repository.loginRequest(userName!!, password!!)
+                var result = repository.loginRequest(email!!, password!!)
                 if (result?.user != null) {
                     listener?.onSuccess("Login succeed ${result?.user!!.uid}")
                 } else {
@@ -37,6 +51,8 @@ class LoginViewModel(
                 listener?.onFinished("login request ${Constants.NETWORK_FINISHED}")
                 user.postValue(result?.user)
             }
+        } else {
+            listener?.onError("Your entered wrong formatted e-Mail or your password less than 6 characters")
         }
     }
 }
